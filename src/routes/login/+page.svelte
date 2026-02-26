be8c7079a49747642667522c0d3f52815026c4e9
JorisBly <joris.bailly.2001@gmail.com>
2026-02-26 12:16:46 +0100

    feat(): Pass load props from server

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import LoginForm from "$lib/components/login-form.svelte";
+    import type { PageData } from "./$types.js";
+    let { data }: { data: PageData } = $props();
 </script>
+
 <div class="flex h-screen w-full items-center justify-center px-4">
-    <LoginForm />
+    <LoginForm {data}/>
 </div>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
     import RegisterForm from "$lib/components/register-form.svelte";
+    import type { PageData } from "./$types.js";
+    let { data }: { data: PageData } = $props();
+
 </script>
 <div class="flex h-screen w-full items-center justify-center px-4">
-    <RegisterForm />
+    <RegisterForm {data} />
 </div>
