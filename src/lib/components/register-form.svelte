1f39a5ef2ac6866d68969f8209ffd67b40210c71
JorisBly <joris.bailly.2001@gmail.com>
2026-02-26 12:19:52 +0100

    feat(): Use superform like the tutorial in shadcn: https://www.shadcn-svelte.com/docs/components/form

--- src/lib/components/register-form.svelte
+++ src/lib/components/register-form.svelte
@@ -2,35 +2,56 @@
     import { Button } from "$lib/components/ui/button/index.js";
     import {Card, CardHeader, CardTitle, CardDescription, CardContent} from "$lib/components/ui/card/index.js";
     import { Input } from "$lib/components/ui/input/index.js";
+    import {FormControl, FormLabel, Field} from "@/components/ui/form";
     import {
         FieldGroup,
-        Field,
-        FieldLabel,
     } from "$lib/components/ui/field/index.js";
-    const id = $props.id();
+    import {formSchema, type FormSchema} from "../../routes/register/schema";
+    import {superForm, type SuperValidated, type Infer,} from "sveltekit-superforms";
+    import {zod4Client} from "sveltekit-superforms/adapters";
+
+    let { data }: { data: { form: SuperValidated<Infer<FormSchema>> } } =
+        $props();
+
+    const form = superForm(data.form, {
+        validators: zod4Client(formSchema),
+    });
+
+    const { form: formData, enhance } = form;
 </script>
+
 <Card class="mx-auto w-full max-w-sm">
     <CardHeader>
         <CardTitle class="text-2xl">Register</CardTitle>
-        <CardDescription>Enter your email below to register a new account</CardDescription>
+        <CardDescription>Enter your email and password below to register a new account</CardDescription>
     </CardHeader>
     <CardContent>
-        <form>
+        <form method="POST" action="/login?/register"  use:enhance>
             <FieldGroup>
-                <Field>
-                    <FieldLabel for="email-{id}">Email</FieldLabel>
-                    <Input id="email-{id}" type="email" placeholder="m@example.com" required />
+                <Field {form} name="email">
+                <FormControl>
+                    {#snippet children({ props })}
+
+                        <FormLabel>Email</FormLabel>
+                        <Input type="email" {...props} bind:value={$formData.email} />
+
+                    {/snippet}
+                </FormControl>
                 </Field>
-                <Field>
-                    <div class="flex items-center">
-                        <FieldLabel for="password-{id}">Password</FieldLabel>
-                    </div>
-                    <Input id="password-{id}" type="password" required />
+
+                <Field {form} name="password">
+                <FormControl>
+                    {#snippet children({ props })}
+                        <FormLabel>Password</FormLabel>
+                        <Input type="password" {...props} bind:value={$formData.password} />
+
+                    {/snippet}
+                </FormControl>
                 </Field>
-                <Field>
+
                     <Button type="submit" class="w-full">Register</Button>
-                </Field>
             </FieldGroup>
         </form>
+
     </CardContent>
 </Card>
