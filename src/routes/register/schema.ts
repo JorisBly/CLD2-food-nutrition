5f960c510a910e5b0d7ddfb8fa18bbc5698c9e1b
JorisBly <joris.bailly.2001@gmail.com>
2026-03-11 11:16:50 +0100

    feat(): Separate the schema of login and reguister because they don't need the same things

--- src/routes/register/schema.ts
+++ src/routes/register/schema.ts
@@ -1,8 +1,12 @@
 import { z } from "zod";
 
-export const formSchema = z.object({
+export const registerSchema = z.object({
+    username: z.string().min(3).max(50),
+    firstname: z.string().min(3).max(50),
+    lastname: z.string().min(3).max(50),
     email: z.string().min(2).max(50),
     password: z.string().min(12).max(50),
 });
 
-export type FormSchema = typeof formSchema;
+export type FormSchema = typeof registerSchema;
+
