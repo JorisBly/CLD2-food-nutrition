81996275ea1aa0594418e82e9b9cf80b5079c1ca
JorisBly <joris.bailly.2001@gmail.com>
2026-03-12 09:27:41 +0100

    feat(): Add a layout for the site

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+    import {SidebarInset, SidebarProvider} from "@/components/ui/sidebar";
+    import AppSidebar from "$lib/components/app-sidebar.svelte";
+    import SiteHeader from "$lib/components/site-header.svelte";
+    import ChartAreaInteractive from "$lib/components/chart-area-interactive.svelte";
 
 	let { children } = $props();
 </script>
 
 <svelte:head>
 </svelte:head>
-{@render children()}
+
+<SidebarProvider
+        style="--sidebar-width: calc(var(--spacing) * 72); --header-height: calc(var(--spacing) * 12);"
+>
+    <AppSidebar variant="inset" />
+    <SidebarInset>
+        <SiteHeader />
+        <div class="flex flex-1 flex-col">
+            <div class="@container/main flex flex-1 flex-col gap-2">
+                <div class="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+                    <div class="px-4 lg:px-6">
+                        {@render children()}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </SidebarInset>
+</SidebarProvider>
+
+
