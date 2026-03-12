dc2a4e6a1123036111a7d7389cd1574e39cf2b21
JorisBly <joris.bailly.2001@gmail.com>
2026-03-12 09:29:58 +0100

    feat(): Create multiple components for dahsboard

--- src/lib/components/app-sidebar.svelte
+++ src/lib/components/app-sidebar.svelte
@@ -1,21 +1,10 @@
 <script lang="ts">
-    import CameraIcon from "@tabler/icons-svelte/icons/camera";
-    import ChartBarIcon from "@tabler/icons-svelte/icons/chart-bar";
-    import DashboardIcon from "@tabler/icons-svelte/icons/dashboard";
-    import DatabaseIcon from "@tabler/icons-svelte/icons/database";
-    import FileAiIcon from "@tabler/icons-svelte/icons/file-ai";
-    import FileDescriptionIcon from "@tabler/icons-svelte/icons/file-description";
-    import FileWordIcon from "@tabler/icons-svelte/icons/file-word";
-    import FolderIcon from "@tabler/icons-svelte/icons/folder";
-    import HelpIcon from "@tabler/icons-svelte/icons/help";
-    import InnerShadowTopIcon from "@tabler/icons-svelte/icons/inner-shadow-top";
-    import ListDetailsIcon from "@tabler/icons-svelte/icons/list-details";
-    import ReportIcon from "@tabler/icons-svelte/icons/report";
-    import SearchIcon from "@tabler/icons-svelte/icons/search";
-    import SettingsIcon from "@tabler/icons-svelte/icons/settings";
-    import UsersIcon from "@tabler/icons-svelte/icons/users";
+    import DashboardIcon from "$lib/icons/dashboard.svelte";
     import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+    import NavMain from "./nav-main.svelte";
     import type { ComponentProps } from "svelte";
+    import WeightIcon from "$lib/icons/weight.svelte";
+
     const data = {
         user: {
             name: "shadcn",
@@ -25,111 +14,14 @@
         navMain: [
             {
                 title: "Dashboard",
-                url: "#",
+                url: "/dashboard",
                 icon: DashboardIcon,
             },
             {
-                title: "Lifecycle",
-                url: "#",
-                icon: ListDetailsIcon,
-            },
-            {
-                title: "Analytics",
-                url: "#",
-                icon: ChartBarIcon,
-            },
-            {
-                title: "Projects",
-                url: "#",
-                icon: FolderIcon,
-            },
-            {
-                title: "Team",
-                url: "#",
-                icon: UsersIcon,
-            },
-        ],
-        navClouds: [
-            {
-                title: "Capture",
-                icon: CameraIcon,
-                isActive: true,
-                url: "#",
-                items: [
-                    {
-                        title: "Active Proposals",
-                        url: "#",
-                    },
-                    {
-                        title: "Archived",
-                        url: "#",
-                    },
-                ],
-            },
-            {
-                title: "Proposal",
-                icon: FileDescriptionIcon,
-                url: "#",
-                items: [
-                    {
-                        title: "Active Proposals",
-                        url: "#",
-                    },
-                    {
-                        title: "Archived",
-                        url: "#",
-                    },
-                ],
-            },
-            {
-                title: "Prompts",
-                icon: FileAiIcon,
-                url: "#",
-                items: [
-                    {
-                        title: "Active Proposals",
-                        url: "#",
-                    },
-                    {
-                        title: "Archived",
-                        url: "#",
-                    },
-                ],
-            },
-        ],
-        navSecondary: [
-            {
-                title: "Settings",
-                url: "#",
-                icon: SettingsIcon,
-            },
-            {
-                title: "Get Help",
-                url: "#",
-                icon: HelpIcon,
-            },
-            {
-                title: "Search",
-                url: "#",
-                icon: SearchIcon,
-            },
-        ],
-        documents: [
-            {
-                name: "Data Library",
-                url: "#",
-                icon: DatabaseIcon,
-            },
-            {
-                name: "Reports",
-                url: "#",
-                icon: ReportIcon,
-            },
-            {
-                name: "Word Assistant",
-                url: "#",
-                icon: FileWordIcon,
-            },
+                title: "Weight diary",
+                url: "/weight",
+                icon: WeightIcon,
+            }
         ],
     };
     let { ...restProps }: ComponentProps<typeof Sidebar.Root> = $props();
@@ -141,8 +33,7 @@
                 <Sidebar.MenuButton class="data-[slot=sidebar-menu-button]:!p-1.5">
                     {#snippet child({ props })}
                         <a href="##" {...props}>
-                            <InnerShadowTopIcon class="!size-5" />
-                            <span class="text-base font-semibold">Acme Inc.</span>
+                            <span class="text-base font-semibold">Nutritrack</span>
                         </a>
                     {/snippet}
                 </Sidebar.MenuButton>
@@ -152,7 +43,4 @@
     <Sidebar.Content>
         <NavMain items={data.navMain} />
     </Sidebar.Content>
-<!--    <Sidebar.Footer>-->
-<!--        <NavUser user={data.user} />-->
-<!--    </Sidebar.Footer>-->
 </Sidebar.Root>
---
+++ src/lib/icons/dashboard.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte';
+</script>
+
+<Icon icon="clarity:dashboard-line" width="36" height="36" />
