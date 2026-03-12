15bd65d747a2618833fc4ba006b71a01309d97b9
JorisBly <joris.bailly.2001@gmail.com>
2026-03-12 09:27:56 +0100

    feat(): Create page server for weight route

---
+++ src/routes/weight/+page.server.ts
@@ -0,0 +1,43 @@
+import {getUserWeights} from "@/server/api";
+import {type Actions, fail, redirect} from "@sveltejs/kit";
+import {superValidate} from "sveltekit-superforms";
+import {zod4} from "sveltekit-superforms/adapters";
+import {weightSchema} from "./schema";
+import {registerSchema} from "../register/schema.ts";
+import {db} from "@/server/db";
+import {weightEntry} from "@/server/db/schema/weight_entry.ts";
+
+
+export async function load({ parent }) {
+    const { user } = await parent()
+    if (!user) {
+        throw new Error("User does not exist");
+    }
+    const weights = await getUserWeights(user.id);
+    return {
+        userId: user.id,
+        weights,
+        form: await superValidate(zod4(registerSchema)),
+    };
+}
+
+export const actions : Actions = {
+    newEntry: async ({ request, locals }) => {
+        const form = await superValidate(request, zod4(weightSchema));
+        if (!form.valid) {
+            return fail(400, {
+                form,
+            });
+        }
+        if (form.data.userId){
+            await db.insert(weightEntry).values({
+                userId: form.data.userId,
+                weight: form.data.weight,
+                date:form.data.date})
+        }else{
+            throw new Error("User not found")
+        }
+
+
+    }
+}
