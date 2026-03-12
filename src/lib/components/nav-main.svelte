b72c92972e3001d679ef51d2197d2b6dcf62782d
JorisBly <joris.bailly.2001@gmail.com>
2026-03-12 11:22:10 +0100

    feat(): Call collapsible component

--- src/lib/components/nav-main.svelte
+++ src/lib/components/nav-main.svelte
@@ -3,6 +3,7 @@
     import type { Icon } from "$lib/icons";
     import {redirect} from "@sveltejs/kit";
     import {goto} from "$app/navigation";
+    import SidebarCollapsible from "./sidebar-collapsible.svelte";
     let { items }: { items: { title: string; url: string; icon?: Icon }[] } = $props();
 </script>
 <Sidebar.Group>
@@ -13,16 +14,21 @@
         </Sidebar.Menu>
         <Sidebar.Menu>
             {#each items as item (item.title)}
-                <Sidebar.MenuItem>
-                    <Sidebar.MenuButton tooltipContent={item.title}>
-                        {#snippet child({ props })}
-                            <a href={item.url} {...props}>
-                                <item.icon />
-                                <span>{item.title}</span>
-                            </a>
-                        {/snippet}
-                    </Sidebar.MenuButton>
-                </Sidebar.MenuItem>
+                {#if item.items}
+                    <SidebarCollapsible item={item} />
+                    {:else}
+                    <Sidebar.MenuItem>
+                        <Sidebar.MenuButton tooltipContent={item.title}>
+                            {#snippet child({ props })}
+                                <a href={item.url} {...props}>
+                                    <item.icon />
+                                    <span>{item.title}</span>
+                                </a>
+                            {/snippet}
+                        </Sidebar.MenuButton>
+                    </Sidebar.MenuItem>
+                    {/if}
+
             {/each}
         </Sidebar.Menu>
     </Sidebar.GroupContent>
