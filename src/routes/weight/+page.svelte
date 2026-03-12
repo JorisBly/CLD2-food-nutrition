ccac5c74733c717c9fdc006c16956ac8199f03fd
JorisBly <joris.bailly.2001@gmail.com>
2026-03-12 09:31:13 +0100

    feat(): Create the page for weight

---
+++ src/routes/weight/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+import DataTable from "@/components/data-table.svelte";
+import {columns} from "./columns";
+import SideSheet from "@/components/side-sheet.svelte";
+import WeightForm from "@/components/weight-form.svelte";
+
+let {data} = $props();
+
+</script>
+
+<SideSheet title="Add an entry" textButton="New entry">
+    <WeightForm {data} />
+</SideSheet>
+<DataTable data={data.weights} {columns}/>
