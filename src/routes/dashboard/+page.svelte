a7311e56a0109fe09ee3c51de46e2ff7665a3fe7
JorisBly <joris.bailly.2001@gmail.com>
2026-03-12 09:28:11 +0100

    feat(): Create dashboard.svelte

---
+++ src/routes/dashboard/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import {SidebarInset, SidebarProvider} from "@/components/ui/sidebar";
+    import AppSidebar from "$lib/components/app-sidebar.svelte";
+    import SiteHeader from "$lib/components/site-header.svelte";
+    import ChartAreaInteractive from "$lib/components/chart-area-interactive.svelte";
+</script>
+
+<ChartAreaInteractive />
