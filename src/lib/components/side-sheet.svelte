f5d160cd204dbd58dc4620e26e51588786e9eaba
JorisBly <joris.bailly.2001@gmail.com>
2026-03-12 09:32:03 +0100

    feat(): Create a side-Sheet for forms

---
+++ src/lib/components/side-sheet.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button/index.js";
+    import * as Sheet from "$lib/components/ui/sheet/index.js";
+    import { buttonVariants } from "$lib/components/ui/button/index.js";
+    import { Input } from "$lib/components/ui/input/index.js";
+    import { Label } from "$lib/components/ui/label/index.js";
+
+    let {textButton, children, title} = $props()
+
+</script>
+
+<Sheet.Root>
+    <Sheet.Trigger class={buttonVariants({ variant: "outline" })}
+    >{textButton}</Sheet.Trigger
+    >
+    <Sheet.Content side="right">
+        <Sheet.Header>
+            <Sheet.Title>{title}</Sheet.Title>
+<!--            <Sheet.Description>-->
+<!--                Make changes to your profile here. Click save when you're done.-->
+<!--            </Sheet.Description>-->
+        </Sheet.Header>
+        {@render children?.()}
+    </Sheet.Content>
+</Sheet.Root>
