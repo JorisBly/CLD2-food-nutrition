2f95556d372ed619bdd0ac7470de0090900d6ba4
JorisBly <joris.bailly.2001@gmail.com>
2026-03-16 08:16:37 +0100

    refactor(): Change secretHAsh from byte to text

--- src/lib/server/db/schema/sessions.ts
+++ src/lib/server/db/schema/sessions.ts
@@ -1,11 +1,10 @@
-import {integer, pgTable, serial, uuid} from 'drizzle-orm/pg-core';
+import { pgTable,text,uuid} from 'drizzle-orm/pg-core';
 import {dates,id} from "@/server/db/schema/columns.helper";
-import {bytes} from "drizzle-orm/gel-core";
 import {users} from "@/server/db/schema/users";
 
 export const sessions = pgTable('sessions', {
     ...id,
     userId: uuid('user_id').references(()=>users.id),
-    secretHash: bytes('secret_hash'),
+    secretHash: text('secret_hash'),
     ...dates
 })
