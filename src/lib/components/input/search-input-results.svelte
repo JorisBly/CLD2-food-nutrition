4ef088ecb1657b6cfe96342e5d594dd39bf5f364
JorisBly <joris.bailly.2001@gmail.com>
2026-03-25 10:51:18 +0100

    fix(): Remove ts from import

--- src/lib/components/input/search-input-results.svelte
+++ src/lib/components/input/search-input-results.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import * as Command from "$lib/components/ui/command/index.js";
+    import * as Command from "$lib/components/ui/command";
     import {Check, Image} from "@lucide/svelte";
     import { cn } from "$lib/utils.js";
     import {Input} from "@/components/ui/input";
